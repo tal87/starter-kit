eae2c30a79582daa9ca340ef87ff5b21f31415a6
Tal Ben Shoam <talbsho@gmail.com>
2019-12-13 22:26:47 +0200

    improve server
    * Use express.static built in method to serve static files
    * Create a default route which will send the index file

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
-const serveStatic = require("serve-static");
 const path = require("path");
 
 //create express app
 const app = express();
 
-//create middleware to handle the serving of the app
-app.use("/", serveStatic(path.join(__dirname, "public")));
+//create middleware to handle the serving of the static files
+app.use("/", express.static(path.join(__dirname, "public")));
+
+// any path will take us to index.html
+// then react router will take care of the path
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/") + "index.html");
+});
 
 // create default port to serve the app
 const port = process.env.PORT || 5000;
